a38b4b93acb08be8b58a7243908c7a7a11df11ce
zacdagostino <zacdagostino@gmail.com>
2024-03-26 23:08:02 +0800

    #Set up the Meta data and gave the layout file the proper html boilerplate.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,20 @@
 import "@styles/globals.css";
 
-const RootLayout = () => {
-  return <div>layout</div>;
+export const metadata = {
+  title: "Zac's Portfolio",
+  description:
+    "Welcome to Zac's Portfolio - showcasing projects, achievements, and expertise in computer science and software engineering.",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div></div>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
 };
 
 export default RootLayout;
